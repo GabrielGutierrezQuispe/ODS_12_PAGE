c5d8f9a48c31e470c9ecafe2908de17e5850752c
GabrielGutierrezQuispe <gabriel.gutierrezq@vallegrande.edu.pe>
2024-11-29 20:50:05 -0500

    limpieza de datos

--- public/js/intro.js
+++
@@ -1,3 +0,0 @@
-setTimeout(function(){
-    window.location.href = "/presentacion";
-}, 5000);
--- public/js/texto.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('load', function() { //Evento que escucha a la hora de cargar la pagina//
-    const textoDerecha = document.querySelector('.texto-derecha'); // Selecciona el div con la clase .texto-derecha
-    textoDerecha.classList.add('appear'); // Añade la clase appear que dispara la animación
-});
